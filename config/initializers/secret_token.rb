bb0e44468db8dea45c02bb29e35856631dad4cb6
Dance Mart <Daniel.Benjamin.Smart@gmail.com>
2013-04-13 10:50:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'df20111f3a407d39246c8e045079f3d18223ccb047033246e1a288d311a9ca08c16c5b09be05e2e21c03c25df2420bf1375444407b59796e31df8d21b2aee3ee'
